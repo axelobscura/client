b49ee0850ce5d4385405f43c1fdd416a3fec9709
Axel Obscura <axosar@gmail.com>
2020-05-12 14:19:49 -0500

    ReduxForm for signup

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 
 class Signup extends Component {
     render() {
         return (
             <form>
                 <fieldset>
-                    <label for="Email">
+                    <label>
                         Email
                     </label>
+                    <Field
+                        name="email"
+                        type="text"
+                        component="input"
+                        autoComplete="none"
+                    />
                 </fieldset>
                 <fieldset>
-                    <label for="Password">
+                    <label>
                         Password
                     </label>
+                    <Field
+                        name="password"
+                        type="password"
+                        component="input"
+                        autoComplete="none"
+                    />
                 </fieldset>
             </form>
         )
     }
 }
 
-export default Signup;
+export default reduxForm({ form: 'signup' })(Signup);
