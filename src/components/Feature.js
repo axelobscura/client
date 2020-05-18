64e70eb383b665028162c809624e602a42edad99
Axel Obscura <axosar@gmail.com>
2020-05-18 09:01:52 -0500

    Feature component

---
+++ src/components/Feature.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Feature extends Component {
+    render() {
+        return (
+            <h2>Feature Data</h2>
+        )
+    }
+};
+
+export default Feature;
